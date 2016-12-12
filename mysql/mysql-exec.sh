7a956c39dc582569748d0c037441487df6b3c680
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2016-12-12 13:26:21 +0000

    Fixes mysql-exec script to allow subsequent calls
    * If script called second time then check for mysql daemon is started
    
    * Avoid using error log for detecting whether mysql is up. Instead use
      socket initialisation.

--- mysql/mysql-exec.sh
+++ mysql/mysql-exec.sh
@@ -16,6 +16,7 @@ MYSQL_USER_PASS=${MYSQL_USER_PASS:-$(pwgen -s 12 1)}
 VOLUME_HOME="/var/lib/mysql"
 ERROR_LOG="/var/log/mysqld.log"
 MYSQLD_PID_FILE="/var/run/mysqld/mysqld.pid"
+EXISTING_MYSQL=false
 
 #########################################################
 # Check in the loop (every 1s) if the database backend
@@ -26,7 +27,7 @@ function wait_for_db() {
   
 	echo "Waiting for DB service..."
 	while true; do
-		if grep "ready for connections" $ERROR_LOG; then
+		if netstat -an | grep "$VOLUME_HOME"; then
 			break;
 		else
 			echo "Still waiting for DB service..." && sleep 1
@@ -48,21 +49,22 @@ function install_db() {
 		echo "=> Installing mysql..."
 		mysql_install_db --user=mysql > /dev/null 2>&1
 		echo "=> Installing mysql... Done!"
+
+        # Move previous error log (which might be there) from previously running container
+        # to different location. We do that to have error log from the currently running
+        # container only.
+        if [ -f $ERROR_LOG ]; then
+            echo "----------------- Previous error log -----------------"
+            tail -n 20 $ERROR_LOG
+            echo "----------------- Previous error log ends -----------------" && echo
+            mv -f $ERROR_LOG "${ERROR_LOG}.old";
+        fi
+
+        touch $ERROR_LOG && chown mysql $ERROR_LOG
 	else
 		echo "=> Using an existing volume of mysql."
+		EXISTING_MYSQL=true
 	fi
-  
-	# Move previous error log (which might be there) from previously running container
-	# to different location. We do that to have error log from the currently running
-	# container only.
-	if [ -f $ERROR_LOG ]; then
-		echo "----------------- Previous error log -----------------"
-		tail -n 20 $ERROR_LOG
-		echo "----------------- Previous error log ends -----------------" && echo
-		mv -f $ERROR_LOG "${ERROR_LOG}.old";
-	fi
-
-	touch $ERROR_LOG && chown mysql $ERROR_LOG
 }
 
 #########################################################
@@ -73,6 +75,10 @@ function install_db() {
 #   $MYSQL_USER_PASS
 #########################################################
 function create_admin_user() {
+	if [ "$EXISTING_MYSQL" = true ]; then
+		return
+	fi
+
 	echo "Creating DB admin user..." && echo
 	local users=$(mysql -s -e "SELECT count(User) FROM mysql.user WHERE User='$MYSQL_USER'")
 	if [[ $users == 0 ]]; then
@@ -95,11 +101,19 @@ function create_admin_user() {
 }
 
 function show_db_status() {
+	if [ "$EXISTING_MYSQL" = true ]; then
+		return
+	fi
+
 	echo "Showing DB status..." && echo
 	mysql -uroot -e "status"
 }
 
 function secure_and_tidy_db() {
+	if [ "$EXISTING_MYSQL" = true ]; then
+		return
+	fi
+
 	echo "Securing and tidying DB..."
 
 	echo "Dropping test db"
@@ -125,7 +139,13 @@ install_db
 
 tail -F $ERROR_LOG & # tail all db logs to stdout 
 
-chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql $VOLUME_HOME
+
+if ps ax -ocomm | grep mysqld_safe | grep -v grep; then
+	echo "mysql daemon already running"
+	exit 0;
+fi
+
 /usr/bin/mysqld_safe & # Launch DB server in the background
 MYSQLD_SAFE_PID=$!
 
